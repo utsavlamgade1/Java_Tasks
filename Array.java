84c65252f374132c4606c7f3759b853193497612
Utsavlamgade1 <utsav.lamgade@codeimmersives.com>
2020-12-03 10:21:06 -0500

    Added new line

--- Array.java
+++ Array.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Array {
     public static void main(String[] args){
         //int [][][] numbers = new int [2][3][4];
-        int [][] numbers = {{1,2,3,4,5,6},{11,22,33,44,55,66}};
-        System.out.println(Arrays.deepToString(numbers));
+        //int [][] numbers = {{1,2,3,4,5,6},{11,22,33,44,55,66}};
+        //System.out.println(Arrays.deepToString(numbers));
+        int [][] newNumber = new int[2][3];
+        System.out.println(Arrays.deepToString(newNumber));
         //System.out.println(Array.toString(numbers));
     }
 
