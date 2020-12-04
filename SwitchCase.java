2861de414abfb13d28bc8315845535ec7af89125
Utsavlamgade1 <utsav.lamgade@codeimmersives.com>
2020-12-04 15:43:12 -0500

    Adding SwitchCase

---
+++ SwitchCase.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the 1-7 in order to choose the day: ");
+        int day = scanner.nextInt();
+
+//      int day = 10;
+
+        switch(day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Enter the numbers in between 1-7.");
+                break;
+        }
+
+    }
+}
