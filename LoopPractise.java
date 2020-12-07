68c825dbc9989d465fe50513b1dabd7d4a032a69
Utsavlamgade1 <utsav.lamgade@codeimmersives.com>
2020-12-07 12:55:24 -0500

    Adding WhileLoop

---
+++ LoopPractise.java
@@ -0,0 +1,18 @@
+package com.codewithUtsav;
+
+public class LoopPractise {
+    public static void main(String[] args){
+        int u = 20;
+        while(u>10){
+            System.out.println(u);
+            u--;
+
+        }
+        System.out.println("************----------------**************");
+        int l=20;
+        while(l<33){
+            System.out.println(l);
+            l++;
+        }
+    }
+}
